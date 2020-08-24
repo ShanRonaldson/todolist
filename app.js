8ddcaed1854d1aa6badc35cc540dd909531c0c29
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-08-24 12:17:54 +0300

    creating to do list files

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 // ECHO is on.
+// jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.get("/", function (req, res) {
+    var today = new Date();
 
-app.get("/", function(req, res){
-    res.send("Hello");
+  if (today.getDay() == 6 || today.getDay() == 0) {
+    res.send("<h1>Yay it's the weekend.</h1>");
+  } else {
+    res.send("<h1>Time to get to work.</h1>");
+  }
 });
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
