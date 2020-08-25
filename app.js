ed5179567a9b39b3733cee8c5fe1922c1c960a1f
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-08-25 11:24:03 +0300

    header/footer creation

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,15 +26,33 @@ app.get("/", function (req, res) {
 
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
   var item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
 
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
   res.redirect("/");
+});
+
+app.get("/about",function(req, res){
+  res.render("about");
 });
 
 app.listen(3000, function () {
